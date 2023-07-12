3ace16315e1c0ddeca83e8b7bbca8312297ede0a
Kevin Figueroa <kevmiguelarronte@gmail.com>
2023-07-12 18:31:51 -0400

    Adding function for woe continous

--- LC_library.py
+++ LC_library.py
@@ -86,6 +86,51 @@ def create_woe_discrete(column_name, X, y):
 
     return {'table': df1, 'info_value': info_value}
 
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def create_woe_continous(column_name, X, y):
+    # Get variable of interest
+    df1 = pd.DataFrame(X[column_name])
+    # Add target variable
+    df1['target'] = y
+
+    # Group data
+    df1 = df1.groupby(column_name).agg({'target': ['count', 'sum']})
+
+    # Rename columns
+    df1.columns = ['observations', 'good_count']
+
+    # Total pct
+    df1['pct_total'] = df1['observations'] / df1['observations'].sum()
+
+    # Get pct of good
+    df1['good_prop'] = df1['good_count'] / df1['good_count'].sum()
+
+    # Calculate bad loan values
+    df1['bad_count'] = df1['observations'] - df1['good_count']
+
+    df1['bad_prop'] = df1['bad_count'] / df1['bad_count'].sum()
+
+    # Rearange columns
+    df1 = df1[['observations', 'pct_total', 'good_count', 'bad_count', 'good_prop', 'bad_prop']]
+
+    # Calculate WoE
+    df1['weight_of_evidence'] = np.log(df1['good_prop'] / df1['bad_prop'])  
+
+    df1['good-bad'] = df1['good_prop'] - df1['bad_prop']
+
+    # Calculate information value
+    df1['info_value'] = np.sum(df1['weight_of_evidence'] * df1['good-bad'])
+    info_value = np.sum(df1['weight_of_evidence'] * df1['good-bad'])
+
+    # Sort table
+    # df1 = df1.sort_values(by='weight_of_evidence')
+
+    # Reset index
+    # df1 = df1.reset_index()
+
+    return {'table': df1, 'info_value': info_value}
+
 # /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 def plot_weight_of_evidence(df, width=12, height=8): 
